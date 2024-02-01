d747b4ac9901100545c66b4207dc26cf1d49d749
Raghavbhati <bhatiraghav4@gmail.com>
2024-02-01 20:11:53 +0530

    Frontend Setup Done

---
+++ Backend/src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser")
+
+const { userRoute } = require("./routes/user.routes");
+const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+app.use(express.json({
+    limit: "16kg"
+}))
+app.use(express.urlencoded({
+    extended : true
+}))
+app.use(express.static("public"));
+app.use(cookieParser())
+
+app.get("/", (req, res)=>{
+    try {
+        res.send("Server is live.. Check API Docs")
+    } catch (error) {
+        console.log("Error occured", error)
+    }
+})
+
+app.use("/user", userRoute)
+
+module.exports = {app}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,9 +3,12 @@ const { connection } = require("./db/db");
 
 require("dotenv").config({path:"./.env"})
 const PORT = process.env.PORT || 8080;
-
+ 
 connection()
 .then(()=>{
+    app.on('error', (error) => {
+        console.error('ERROR:', error);
+    });
     app.listen(PORT, ()=>{
         console.log(`Server is live on port no. ${PORT}`)
     })
