4f76f1e25eba61c4e0c92236833dd39425bdd793
Raghavbhati <bhatiraghav4@gmail.com>
2024-02-02 18:27:28 +0530

    Multer added

---
+++ Backend/src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+const { v2: cloudinary } = require("cloudinary");
+const fs = require("node:fs");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const uploadOnCloudinary = async (localPath)=>{
+    try {
+        if(!localPath) return null
+        const res = cloudinary.uploader.upload(localPath,{
+            resource_type : "auto",
+        })
+        console.log("uploaded on the cloudinary", res)
+        return res;
+    } catch (error) {
+        fs.unlinkSync(localPath)
+        return null
+    }
+}
+
+module.exports = {uploadOnCloudinary}
